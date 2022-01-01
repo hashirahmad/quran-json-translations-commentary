e76eeb640326f356319b4b04550453eca5a209b5
Hashir <hashir@coinmode.com>
2022-01-01 08:27:25 +0000

    Further corrections of content and scripts.

--- content/uniqueTopics.js
+++ content/uniqueTopics.js
@@ -2,32 +2,47 @@ const path = require('path')
 const jsonfile = require('jsonfile')
 const allTopics = require('./json/allTopics.json')
 
+/**
+ * It will get all the relevant verses mentioning given
+ * `topic`.
+ */
+async function getMaxVerses({ topic }) {
+    const all = []
+    const file = path.join(__dirname, 'json/allOfQuran.json')
+    const allOfQuran = await jsonfile.readFile(file)
+    for (let j = 0; j < allOfQuran.length; j += 1) {
+        const verse = allOfQuran[j]
+        const filtered = verse.topics.filter((o) => o.topic === topic)
+        if (filtered.length) {
+            all.push(verse)
+        }
+    }
+    return {
+        length: all.length,
+        verses: all,
+    }
+}
+
 async function run() {
     const topics = {}
+    const topicsArray = []
     for (let i = 0; i < allTopics.length; i += 1) {
         const topic = allTopics[i]
-        const exists = topics[topic.topic]
-        if (exists) {
-            exists.id += topic.id
-            if (topic.verses) {
-                exists.verses.push(...topic.verses.split(','))
-            }
-        } else {
-            topics[topic.topic] = {
-                id: topic.id,
-                verses: (topic.verses && topic.verses.split(',')) || [],
-            }
+        topicsArray.push(topic.topic)
+        console.log('Doing', topic.topic)
+    }
+    const uniqueTopics = [...new Set(topicsArray)]
+    for (let i = 0; i < uniqueTopics.length; i += 1) {
+        const topic = uniqueTopics[i]
+        // eslint-disable-next-line no-await-in-loop
+        const r = await getMaxVerses({ topic })
+        topics[topic] = {
+            id: i,
+            maxVerses: r.length,
         }
-        console.log('Doing:', topic.topic)
+        console.log('Getting max verses for', topic)
     }
-    // eslint-disable-next-line guard-for-in, no-restricted-syntax
-    for (const key in topics) {
-        const { verses } = topics[key]
-        const unique = [...new Set(verses)]
-        const maxVerses = unique.length || 1
-        delete topics[key].verses
-        topics[key].maxVerses = maxVerses
-    }
+
     const filepath = path.join(__dirname, '/json/topics.json')
     await jsonfile.writeFile(filepath, topics, { spaces: 4 })
 }
